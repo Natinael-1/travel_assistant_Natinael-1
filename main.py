6ecf5bb3d13562a2f5d404ceb63f2c67b486c3a2
Natinael-1 <n.borana@alustudent.com>
2025-11-19 19:35:24 +0200

    created file

---
+++ main.py
@@ -0,0 +1,7 @@
+#Here is the main python file
+
+print("\n🙋🙋Welcome to Your Travel Assistant  🙋‍♀️🙋‍♀️")
+print("===Please choose what you want below===.")
+while True:
+    choice = input("Please enter city you want to travel to: ")
+
